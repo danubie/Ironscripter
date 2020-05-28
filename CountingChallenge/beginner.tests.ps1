b8b6706409fd4fc704e9458944fec04d788f7f8f
Wolfgang Wagner (danubie) <w_wagner62@hotmail.com>
2020-05-28 19:10:20 +0200

    change test cases 2, 4, 10 to 2, 10, 100

--- CountingChallenge/beginner.tests.ps1
+++ CountingChallenge/beginner.tests.ps1
@@ -3,43 +3,46 @@ Describe "Test SumEvenNumbersCStyleForLoop" {
     It "Edge Case Last=2 -> 2=2" {
         SumEvenNumbersCStyleForLoop -Last 2 | Should -Be 2
     }
-    It "Last = 4 -> 2+4=6" {
+    It "Last = 10 -> 2+4+6+8+10=30" {
         SumEvenNumbersCStyleForLoop -Last 10 | Should -Be 30
     }
-    It "Last = 10 -> 2+4+6+8+10=30" {
+    It "Last = 100 -> 2+4+6+8+10..100=2550" {
         SumEvenNumbersCStyleForLoop -Last 100 | Should -Be 2550
     }
 }
+
 Describe "Test SumGaußIncrement2" {
     It "Edge Case Last=2 -> 2=2" {
         SumGaußIncrement2 -Last 2 | Should -Be 2
     }
-    It "Last = 4 -> 2+4=6" {
+    It "Last = 10 -> 30" {
         SumGaußIncrement2 -Last 10 | Should -Be 30
     }
-    It "Last = 10 -> 2+4+6+8+10=30" {
+    It "Last = 100 -> 2550" {
         SumGaußIncrement2 -Last 100 | Should -Be 2550
     }
 }
+
 Describe "Test SumGaußIncrement2b" {
     It "Edge Case Last=2 -> 2=2" {
         SumGaußIncrement2b -Last 2 | Should -Be 2
     }
-    It "Last = 4 -> 2+4=6" {
+    It "Last = 10 -> 30" {
         SumGaußIncrement2 -Last 10 | Should -Be 30
     }
-    It "Last = 10 -> 2+4+6+8+10=30" {
+    It "Last = 100 -> 2550" {
         SumGaußIncrement2b -Last 100 | Should -Be 2550
     }
 }
+
 Describe "Test SumGaußIncrement2Bitwise" {
     It "Edge Case Last=2 -> 2=2" {
         SumGaußIncrement2Bitwise -Last 2 | Should -Be 2
     }
-    It "Last = 4 -> 2+4=6" {
+    It "Last = 10 -> 30" {
         SumGaußIncrement2Bitwise -Last 10 | Should -Be 30
     }
-    It "Last = 10 -> 2+4+6+8+10=30" {
+    It "Last = 100 -> 2550" {
         SumGaußIncrement2Bitwise -Last 100 | Should -Be 2550
     }
 }
