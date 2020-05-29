a9651cbf33b908e0cb32d7145e782e6383f70d35
Wolfgang Wagner (danubie) <w_wagner62@hotmail.com>
2020-05-29 21:29:51 +0200

    consitent naming and dates

--- ObjectAge/Get-ObjectAge.tests.ps1
+++ ObjectAge/Get-ObjectAge.tests.ps1
@@ -18,23 +18,23 @@ Describe "Simple tests using get-date" {
 Describe "Testing with files" {
     It "array by pipeline" {
         # Arrange  testdata
-        $file1 = New-Item -Path 'Testdrive:\File1' -ItemType File
-        $file2 = New-Item -Path 'Testdrive:\File2' -ItemType File
-        $file3 = New-Item -Path 'Testdrive:\File3' -ItemType File
-        $file2.CreationTime = $file1.CreationTime.AddDays(1)
-        $file3.CreationTime = $file1.CreationTime.AddDays(2)
+        $file0 = New-Item -Path 'Testdrive:\File1' -ItemType File
+        $file1 = New-Item -Path 'Testdrive:\File2' -ItemType File
+        $file2 = New-Item -Path 'Testdrive:\File3' -ItemType File
+        $file1.CreationTime = $file1.CreationTime.AddDays(-1)
+        $file2.CreationTime = $file1.CreationTime.AddDays(-2)
 
         # Act testing
         $ret = Get-ChildItem -Path 'TestDrive:\' |
             Get-ObjectAge -CreateDateProperty 'CreationTime' -ModifyDateProperty 'LastWriteTime'
 
         # Assert results
-        $ret[0].CreationTime | Should -Be $file1.CreationTime
-        $ret[1].CreationTime | Should -Be $file2.CreationTime
-        $ret[2].CreationTime | Should -Be $file3.CreationTime
-        $ret[0].AboveAvg | Should -Be $false
-        $ret[1].AboveAvg | Should -Be $false
-        $ret[2].AboveAvg | Should -Be $true
+        $ret[0].CreationTime | Should -Be $file0.CreationTime
+        $ret[1].CreationTime | Should -Be $file1.CreationTime
+        $ret[2].CreationTime | Should -Be $file2.CreationTime
+        $ret[0].AboveAvg | Should -Be $true
+        $ret[1].AboveAvg | Should -Be $true
+        $ret[2].AboveAvg | Should -Be $false
     }
 }
 
